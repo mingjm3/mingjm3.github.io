bc6b4303429382ed0fc094ea4f4611c200024108
Arcgo <imingjun.ma@outlook.com>
2023-01-26 19:10:14 -0800

    achieve nav bar

---
+++ my-website/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Nav() {
+    return (
+        <nav>
+            <ol>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#resume">Resume</a></li>
+            </ol>
+        </nav>
+    )
+}
