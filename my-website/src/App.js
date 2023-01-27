dc41f7bd976a54dde4ac1b27ef69480ef5d7f441
Arcgo <imingjun.ma@outlook.com>
2023-01-26 19:12:23 -0800

    delete default code and write new structure

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navbar.js'
+import About from './components/About.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
       </header>
+      <div className="App-body">
+        <section id='about'>
+          <About />
+        </section>
+        <section id='experience'>
+          <h2>Experience</h2>
+        </section>
+        <section id='projects'>
+          <h2>Projects</h2>
+        </section>
+        <section id='contact'>
+          <h2>Contact</h2>
+        </section>
+        <section id='resume'>
+          <h2>Resume</h2>
+        </section>
+      </div>
     </div>
   );
 }
