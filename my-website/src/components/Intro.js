6adcfc6ea0caaaffc1737ea14576f25af9235450
Arcgo <imingjun.ma@outlook.com>
2023-01-27 16:36:30 -0800

    achieve intro

--- my-website/src/components/Intro.js
+++ my-website/src/components/Intro.js
@@ -1,11 +1,12 @@
 import Socialbar from "./Socialbar";
+import './Intro.css';
 
 export default function Intro() {
     return (
-        <div>
+        <div className="intro-page">
             <h1>Hi, my name is </h1>
             <h2>Mingjun Ma</h2>
-            <p>I am a full-stack developer.</p>
+            <h3>M.S. in Computer Science | Software Engineer</h3>
             <Socialbar />
         </div>
     );
