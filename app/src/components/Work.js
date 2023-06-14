f5854994418b0483fc396f8d043fd5bce381de29
Arcgo <imingjun.ma@outlook.com>
2023-06-13 18:56:08 -0700

    [fix] link in work is null and deploy

--- app/src/components/Work.js
+++ app/src/components/Work.js
@@ -20,6 +20,7 @@ const data = [
     endDate: "Aug 2022",
     description: ["- Deployed a U.S. oil company business on Azure using Docker, Kubernetes and MongoDB, resulting in a 50% reduction in infrastructure costs and a 30% increase in system scalability.", "- Achieved and tested order REST APIs, enabling seamless order tracking, updates, and creation using SpringBoot and Jmeter, resulting in a 20% improvement in order processing efficiency and a 15% decrease in error rates.", "- Designed and implemented a robust CI/CD pipeline using Github Actions, reducing deployment time by 40% and ensuring continuous integration and delivery of software updates with zero downtime."],
     technologies: ["Java", "SpringBoot", "Azure", "REST APIs", "CI/CD", "Docker", "Kubernetes", "MongoDB", "GitHub Actions", "Jenkins"],
+    url: "https://www.thoughtworks.com/en-us"
   },
   {
     logo: "https://media.licdn.com/dms/image/C4D0BAQFWMwEmsK0Aeg/company-logo_200_200/0/1519952553348?e=1694649600&v=beta&t=DrKjv5JEMrdNboWYHA3lPeuGePwdJWkjtomtPZWE2Wk",
@@ -28,7 +29,8 @@ const data = [
     startDate: "Aug 2021",
     endDate: "Sep 2021",
     description: ["- Achieved dynamic routing algorithms implementation in a commercial router, resulting in an improvement in network efficiency and reduced packet loss by C++", "- Designed and developed test automation frameworks to assess router throughput and routing accuracy. Utilized Python to automate test scenarios, resulting in a 30% reduction in testing time."],
-    technologies: ["Python", "C++"]
+    technologies: ["Python", "C++"],
+    url: "https://www.tp-link.com/us/"
   },
   {
     logo: "https://media.licdn.com/dms/image/C560BAQH784Gt17OeVg/company-logo_200_200/0/1643080249339?e=1694649600&v=beta&t=9HaJXaRp0h2tGMoQm0aqvKrHs-u0rLK26x2_ynzcfDs",
@@ -37,7 +39,8 @@ const data = [
     startDate: "Jun 2020",
     endDate: "2Jul 2021",
     description: ["- Participated in intensive daily clinical practice in the hospital; prescribing medications, eyeglasses and contact lenses accumulating 600 cases; carried out practical diagnostics accumulating 800 times, such as myopia and glaucoma, etc.", "- Led research in human stereoscopic vision and found binocular disparity, degree of myopia, and strabismus are the main factors affecting stereopsis.", "- Worked in a visual electrophysiology lab, recorded the effects of images on visual electrophysiology and psychological vision."],
-    technologies: ["Research", "Vision Science"]
+    technologies: ["Research", "Vision Science"],
+    url:"https://www.wchscu.cn/Home.html"
   }
 ];
 
